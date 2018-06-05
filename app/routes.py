5c499bf8ba1cb88f644cfd9632150c177eecf07d
mmarcano22 <manuelmarcano22@yahoo.com>
2018-06-05 15:52:38 -0500

    :octocat: fixed glob on routes

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,11 @@ from app.compute import compute, bplot
 from bokeh.util.string import encode_utf8
 import glob
 from random import randint
-import re
 
 #Error handler
 @app.errorhandler(404)
 def page_not_found(e):
-    lista = glob.glob('app/static/Jokes/*.jpg')
+    lista = glob.glob('./**/static/Jokes/*.jpg', recursive=True)
     ra = randint(0,len(lista))
     names = lista[ra].split('/')[-1]
     return render_template('404.html',name=names), 404
--- gunicornrun.sh
+++ gunicornrun.sh
@@ -2,3 +2,4 @@
 export FLASK_APP=calc.py
 #flask run -h 0.0.0.0
 gunicorn -b calc.manuelpm.me:5000  -w 4 app:app
+#gunicorn -b localhost:5000  -w 4 app:app
