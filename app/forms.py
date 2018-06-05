441e7657d846a31d17960fbdb1587961f9edda18
mmarcano22 <manuelmarcano22@yahoo.com>
2018-06-05 17:10:58 -0500

    Bootstratp work :octocat:

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, FloatField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, InputRequired
 import numpy as np
 
 class LoginForm(FlaskForm):
@@ -12,10 +12,10 @@ class LoginForm(FlaskForm):
 
 
 class InputForm(FlaskForm):
-    A = FloatField('Amplitude (m)', default=1.0, validators=[DataRequired()])
-    b = FloatField('Dampling Factor (kg/s)', default=0)
-    w = FloatField('Freq (1/s)', default=2*np.pi, validators=[DataRequired()])
-    T = FloatField('Time (s)', default=18.0, validators=[DataRequired()])
+    A = FloatField('Amplitude (m)', default=1.0, description = "Amplitude algo" ,validators=[InputRequired()])
+    b = FloatField('Dampling Factor (kg/s)',  description = "Damp the thing", default=0)
+    w = FloatField('Freq (1/s)', description = "Hertz" ,default=2*np.pi, validators=[InputRequired()])
+    T = FloatField('Time (s)', description = "Self Explanatory" ,default=18.0, validators=[InputRequired()])
     #submit = SubmitField('Compute')
 
 
